7239abda469035dfb9aa5f036fdf118b7c956806
arsnazarenko <arsnazarenko@gmail.com>
2024-08-15 17:49:22 +0300

    Add worker pool
    Signed-off-by: arsnazarenko <arsnazarenko@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,92 +1,97 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
 const (
-	commonUrl string = "https://enroll.spbstu.ru/applications-manager/api/v1/admission-list/form-rating?applicationEducationLevel=%s&directioneducationformid=%d&directionId=%d"
+	commonUrl  string = "https://enroll.spbstu.ru/applications-manager/api/v1/admission-list/form-rating?applicationEducationLevel=%s&directioneducationformid=%d&directionId=%d"
 )
 
-type EducationLevel string
+type (
+	EducationFormId uint8
+	EducationLevel  string
+)
 
 const (
 	EducationLevelBachelor EducationLevel = "BACHELOR" // Бакалавриат
 	EducationLevelMaster   EducationLevel = "MASTER"   // Магистратура
 	EducationLevelGraduate EducationLevel = "GRADUATE" // Аспирантура
-)
 
-type EducationFormId uint8
-
-const (
 	EducationFormIdCorrespondence EducationFormId = iota + 1 // Заочное
 	EducationFormIdFullTime                                  // Очное
 	EducationFormIdPartTime                                  // Очно-заочное
 )
 
-type Subject struct {
-	Title      string `json:"title"`
-	ExternalId string `json:"externalId"`
-	Score      uint16 `json:"score"`
-}
-type DirectionEducationForm struct {
-	Id           uint64 `json:"id"`
-	Title        string `json:"title"`
-	TitleForeign string `json:"titleForeign"`
-	ExternalId   string `json:"externalId"`
-}
+type (
+	Subject struct {
+		Title      string `json:"title"`
+		ExternalId string `json:"externalId"`
+		Score      uint16 `json:"score"`
+	}
+	DirectionEducationForm struct {
+		Id           uint64 `json:"id"`
+		Title        string `json:"title"`
+		TitleForeign string `json:"titleForeign"`
+		ExternalId   string `json:"externalId"`
+	}
 
-type DirectionPaymentForm struct {
-	Id           uint64 `json:"id"`
-	Title        string `json:"title"`
-	TitleForeign string `json:"titleForeign"`
-	ExternalId   string `json:"externalId"`
-}
+	DirectionPaymentForm struct {
+		Id           uint64 `json:"id"`
+		Title        string `json:"title"`
+		TitleForeign string `json:"titleForeign"`
+		ExternalId   string `json:"externalId"`
+	}
 
-type User struct {
-	ApplicationEducationLevel string `json:"applicationEducationLevel"`
+	User struct {
+		ApplicationEducationLevel string `json:"applicationEducationLevel"`
 
-	DirectionEducationForm DirectionEducationForm `json:"directionEducationForm"`
-	DirectionPaymentForm   DirectionPaymentForm   `json:"directionPaymentForm"`
+		DirectionEducationForm DirectionEducationForm `json:"directionEducationForm"`
+		DirectionPaymentForm   DirectionPaymentForm   `json:"directionPaymentForm"`
 
-	DirectionId uint64 `json:"directionId"`
+		DirectionId uint64 `json:"directionId"`
 
-	Subjects []Subject `json:"subjects"`
+		Subjects []Subject `json:"subjects"`
 
-	UserFullName              string  `json:"userFullName"`
-	UserSnils                 string  `json:"userSnils"`
-	UserUniqueId              string  `json:"userUniqueId"`
-	UserExternalId            string  `json:"userExternalId"`
-	Priority                  uint16  `json:"priority"`
-	WithoutExam               bool    `json:"withoutExam"`
-	FullScore                 uint16  `json:"fullScore"`
-	SubjectScore              uint16  `json:"subjectScore"`
-	HasFeature                bool    `json:"hasFeature"`
-	HasAgreement              bool    `json:"hasAgreement"`
-	HasOriginalDocuments      bool    `json:"hasOriginalDocuments"`
-	AchievementScore          uint16  `json:"achievementScore"`
-	AchievementScoreExtra     uint16  `json:"achievementScoreExtra"`
-	HasSpecialFeature         bool    `json:"hasSpecialFeature"`
-	HasAchievement            bool    `json:"hasAchievement"`
-	HasOlympiad               bool    `json:"hasOlympiad"`
-	HasOlympiadReset          bool    `json:"hasOlympiadReset"`
-	HasGovernmentContract     bool    `json:"hasGovernmentContract"`
-	NeedDormitory             bool    `json:"needDormitory"`
-	CertificateAverage        float32 `json:"certificateAverage"`
-	CertificateProfileAverage float32 `json:"certificateProfileAverage"`
-	State                     string  `json:"state"`
-}
+		UserFullName              string  `json:"userFullName"`
+		UserSnils                 string  `json:"userSnils"`
+		UserUniqueId              string  `json:"userUniqueId"`
+		UserExternalId            string  `json:"userExternalId"`
+		Priority                  uint16  `json:"priority"`
+		WithoutExam               bool    `json:"withoutExam"`
+		FullScore                 uint16  `json:"fullScore"`
+		SubjectScore              uint16  `json:"subjectScore"`
+		HasFeature                bool    `json:"hasFeature"`
+		HasAgreement              bool    `json:"hasAgreement"`
+		HasOriginalDocuments      bool    `json:"hasOriginalDocuments"`
+		AchievementScore          uint16  `json:"achievementScore"`
+		AchievementScoreExtra     uint16  `json:"achievementScoreExtra"`
+		HasSpecialFeature         bool    `json:"hasSpecialFeature"`
+		HasAchievement            bool    `json:"hasAchievement"`
+		HasOlympiad               bool    `json:"hasOlympiad"`
+		HasOlympiadReset          bool    `json:"hasOlympiadReset"`
+		HasGovernmentContract     bool    `json:"hasGovernmentContract"`
+		NeedDormitory             bool    `json:"needDormitory"`
+		CertificateAverage        float32 `json:"certificateAverage"`
+		CertificateProfileAverage float32 `json:"certificateProfileAverage"`
+		State                     string  `json:"state"`
+	}
 
-type Response struct {
-	Users              []User `json:"list"`
-	Log                string `json:"-"`
-	DirectionCapacity  uint64 `json:"-"`
-	Total              uint64 `json:"-"`
-	TotalWithOriginals uint64 `json:"-"`
-}
+	Response struct {
+		Users              []User `json:"list"`
+		Log                string `json:"-"`
+		DirectionCapacity  uint64 `json:"-"`
+		Total              uint64 `json:"-"`
+		TotalWithOriginals uint64 `json:"-"`
+	}
+)
 
 var (
 	headers = map[string]string{
@@ -96,16 +101,15 @@ var (
 	}
 )
 
-func GetCompetitionList(eduLevel EducationLevel, eduForm EducationFormId, directionId uint64) ([]User, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf(commonUrl, eduLevel, eduForm, directionId), nil)
+func GetCompetitionList(ctx context.Context, url string) ([]User, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
 	for k, v := range headers {
 		req.Header.Set(k, v)
 	}
-	client := http.DefaultClient
-	resp, err := client.Do(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
@@ -121,12 +125,83 @@ func GetCompetitionList(eduLevel EducationLevel, eduForm EducationFormId, direct
 	}
 	return resultResp.Users, nil
 }
+const (
+    firstDirID = 0
+    lastDirID = 303
+    maxWorkers = 8
+)
+
+
+type Result struct {
+    Users []User
+    Err error
+}
+
+
+func Worker(ctx context.Context, wg *sync.WaitGroup, inChan <-chan string, outChan chan<- Result) {
+    defer wg.Done()
+    for url := range inChan {
+        users, err := GetCompetitionList(ctx, url)
+        outChan <- Result{
+        	Users: users,
+        	Err:   err,
+        }
+    } 
+}
+
+type Snils string
+type UserDb map[Snils][]*User
+
+func (db UserDb) addUserRow(user *User) {
+    db[Snils(user.UserSnils)] = append(db[Snils(user.UserSnils)], user)
+}
+func (db UserDb) PrinUserRow(user *User) {
+    row, ok := db[Snils(user.UserSnils)]
+    if !ok {
+        fmt.Printf("User not found\n")
+        return
+    }
+    
+    fmt.Printf("User: %s\n", user.UserSnils)
+    for _, u := range row {
+        fmt.Printf("Специальность: %s, сумма баллов: %d, приоритет: %s, позиция в списке: %d, оригинал: %t\n", u.DirectionEducationForm.TitleForeign)
+    }
+}
 
 func main() {
-	users, err := GetCompetitionList(EducationLevelMaster, EducationFormIdFullTime, 307)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(users)
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second * 30)
+    reqChan := make(chan string, maxWorkers)
+    resultChan := make(chan Result, maxWorkers)
+    wg := &sync.WaitGroup{}
+    db := make(UserDb, 4096)
+    defer cancel()
+
+    
+    for range maxWorkers {
+        go Worker(ctx, wg, reqChan, resultChan)
+    }
+
+    for dirID := firstDirID; dirID <= lastDirID; dirID++ {
+       reqChan <- fmt.Sprintf(commonUrl, EducationLevelMaster, EducationFormIdFullTime, dirID)
+    } 
+    close(reqChan)
+
+    wg.Wait()
+    close(resultChan)
+
+    for res := range resultChan {
+        if res.Err != nil {
+            fmt.Fprintf(os.Stderr, "error occured while making request: %v", res.Err)
+        } else {
+            for _, u := range res.Users {
+                db.addUserRow(&u)
+            }    
+        }
+    }
+
+
+
 
 }
+
+
